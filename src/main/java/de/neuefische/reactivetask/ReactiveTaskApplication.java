7818aee1e778762d9e76ba7efe391eca87036f42
André Schreck <andre.schreck@gmail.com>
2021-10-23 23:35:33 +0200

    first commit

---
+++ src/main/java/de/neuefische/reactivetask/ReactiveTaskApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.reactivetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/neuefische/reactivetask/ReactiveTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.reactivetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
